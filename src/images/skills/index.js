71a2774b91e298b5864f20c716f05e01bf79ecef
SirMrTyler <sirmrtyler.business@gmail.com>
2026-02-26 19:02:16 -0800

    fixing build issues
    Had to created importable/exportable index.js file within the images/skills dir to import the skills on one line

---
+++ src/images/skills/index.js
@@ -0,0 +1,16 @@
+export { default as blender } from "./blender.svg";
+export { default as cPlus } from "./cPlus.svg";
+export { default as cSharp } from "./cSharp.svg";
+export { default as css3 } from "./css.svg";
+
+export { default as dotNet } from "./dotNet.svg";
+export { default as dotNetCore } from "./dotNetCore.svg";
+export { default as git } from "./git.svg";
+export { default as html5 } from "./html5.svg";
+export { default as jquery } from "./jquery.svg";
+export { default as js } from "./js.svg";
+export { default as react } from "./react.svg";
+export { default as sass } from "./sass.svg";
+export { default as sql } from "./sql.svg";
+export { default as sqlServer } from "./sqlServer.svg";
+export { default as unreal } from "./unreal.svg";
